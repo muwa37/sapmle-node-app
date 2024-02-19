00c72c78070d85e95513dfd8790c5ac47b0ab40f
muwa37 <yond41m3@icloud.com>
2024-02-20 00:32:32 +0600

    Added mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const userRouter = require('./src/user-router');
 const Application = require('./app/Application');
 const jsonParser = require('./app/parseJson');
 const parseUrl = require('./app/parseUrl');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
 const app = new Application();
@@ -9,6 +10,16 @@ const app = new Application();
 app.use(jsonParser);
 app.use(parseUrl('http://localhost:5000'));
 app.addRouter(userRouter);
-app.listen(PORT, () => {
-  console.log(`serv started on ${PORT}`);
-});
+
+const start = async () => {
+  try {
+    await mongoose.connect();
+    app.listen(PORT, () => {
+      console.log(`serv started on ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
--- src/user-controller.js
+++ src/user-controller.js
@@ -1,18 +1,27 @@
-const users = [
-  { id: 1, name: 'sample' },
-  { id: 2, name: 'example' },
-];
+// const users = [
+//   { id: 1, name: 'sample' },
+//   { id: 2, name: 'example' },
+// ];
 
-const getUsers = (req, res) => {
+const User = require('./user-model');
+
+const getUsers = async (req, res) => {
+  // if (req.params.id) {
+  //   return res.send(users.find(user => user.id == req.params.id));
+  // }
+  let users;
   if (req.params.id) {
-    return res.send(users.find(user => user.id == req.params.id));
+    users = await User.findById(req.params.id);
+  } else {
+    users = await User.find();
   }
   res.send(users);
 };
 
-const createUser = (req, res) => {
-  const user = req.body;
-  users.push(user);
+const createUser = async (req, res) => {
+  // const user = req.body;
+  // users.push(user);
+  const user = await User.create(req.body);
   res.send(users);
 };
 
